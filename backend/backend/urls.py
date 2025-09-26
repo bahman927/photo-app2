078b6727f6142160eb6207ebaf0cc2c5a5c06406
Bahman Dorand <bahman927@gmail.com>
2025-09-26 00:11:06 -0400

    update backend.api.urls.py

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -2,10 +2,15 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.urls import path, include
 from django.contrib import admin
+from django.http import HttpResponse
+
+def home(request):
+    return HttpResponse("✅ Django is running on Render!")
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("backend.api.urls")),   # replace with your app
+     path("", home),   
 ]
 
 # Serve static + media in development
